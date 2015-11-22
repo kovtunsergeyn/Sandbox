f8c14951435c611cc097e2a472b9f9ca6586a4b5
Kovtun Sergei <s.kovtun@bars-open.ru>
2015-11-22 14:47:05 +0300

    finished head first javascript

--- JS examples/chapter12/youcube/date.js
+++ JS examples/chapter12/youcube/date.js
@@ -1,4 +1,4 @@
 // Custom Date function to display a date in MM/DD/YYYY format
-Date.prototype.shortFormat = function() {
-  return (this.getMonth() + 1) + "/" + this.getDate() + "/" + this.getFullYear();
+Date.prototype.shortFormat = function () {
+    return (this.getMonth() + 1) + "/" + this.getDate() + "/" + this.getFullYear();
 }
